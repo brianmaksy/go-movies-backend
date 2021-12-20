53913c4ddb2d644d60774ffe8a8c755fd01f6cab
Brian <brian.mak.sy@gmail.com>
2021-12-20 10:29:41 +0000

    After securing routes

---
+++ cmd/api/tokens.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"backend/models"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/pascaldekloe/jwt"
+	"golang.org/x/crypto/bcrypt"
+)
+
+// nts - simulate retrieving valid user from database
+var validUser = models.User{
+	ID:       10,
+	Email:    "me@here.com",
+	Password: "$2a$12$DkT9TUoOjOouV5AeVdC17OZWF6p2E17CSrh8dWzyNvDbcSOJtH1KC",
+}
+
+type Credentials struct {
+	Username string `json:"email"`
+	Password string `json:"password"`
+}
+
+func (app *application) Signin(w http.ResponseWriter, r *http.Request) {
+	var creds Credentials
+
+	err := json.NewDecoder(r.Body).Decode(&creds) // decode into creds
+	if err != nil {
+		app.errorJSON(w, errors.New("unauthorized"))
+		return
+	}
+
+	hashedPassword := validUser.Password
+
+	err = bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(creds.Password))
+	if err != nil {
+		app.errorJSON(w, errors.New("unauthorized"))
+		return
+	}
+	var claims jwt.Claims
+	claims.Subject = fmt.Sprint(validUser.ID)
+	claims.Issued = jwt.NewNumericTime(time.Now())
+	claims.NotBefore = jwt.NewNumericTime(time.Now())
+	claims.Expires = jwt.NewNumericTime(time.Now().Add(24 * time.Hour))
+	claims.Issuer = "mydomain.com"              // domain name
+	claims.Audiences = []string{"mydomain.com"} // to access from different remote sites
+
+	jwtBytes, err := claims.HMACSign(jwt.HS256, []byte(app.config.jwt.secret)) // signs our token
+	if err != nil {
+		app.errorJSON(w, errors.New("error signing"))
+		return
+	}
+
+	app.writeJSON(w, http.StatusOK, string(jwtBytes), "response")
+}
